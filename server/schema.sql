2462f851d65ab60d491ed2665299620e6de4e42e
Chris Hesterman <chrishesterman14@gmail.com>
2020-04-22 12:23:18 -0700

    "Complete designing multi-table schema"

--- server/schema.sql
+++ server/schema.sql
@@ -1,23 +1,43 @@
-CREATE DATABASE chat;
+DROP DATABASE IF EXISTS `chat`;
 
-USE chat;
+CREATE DATABASE `chat`;
 
-CREATE TABLE messages (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  message TINYTEXT,
-  id_users INT,
-  id_rooms INT,
-  timestamp DATETIME
+USE `chat`;
+
+CREATE TABLE `messages` (
+  `id` INT AUTO_INCREMENT,
+  `message` TINYTEXT NOT NULL,
+  `users_id_user` INT NOT NULL,
+  `rooms_id_room` INT NOT NULL,
+  `timestamp` DATETIME NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  username TEXT(25),
-  current_room INT,
+CREATE TABLE `users` (
+  `id` INT AUTO_INCREMENT,
+  `username` VARCHAR(25) NOT NULL,
+  `rooms_id_currentRoom` INT NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
+CREATE TABLE `rooms` (
+  `id` INT AUTO_INCREMENT,
+  `roomname` VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `friends` (
+  `id` INT AUTO_INCREMENT,
+  `users_id_user` INT NOT NULL,
+  `users_id_friend` INT NOT NULL,
+  PRIMARY KEY (`id`)
+);
 /* Create other tables and define schemas for them here! */
-
+ALTER TABLE `users` ADD FOREIGN KEY(`rooms_id_currentRoom`) REFERENCES `rooms` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY(`users_id_user`) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY(`rooms_id_room`) REFERENCES `rooms` (`id`);
+ALTER TABLE `friends` ADD FOREIGN KEY(`users_id_user`) REFERENCES `users` (`id`);
+ALTER TABLE `friends` ADD FOREIGN KEY(`users_id_friend`) REFERENCES `users` (`id`);
 
 
 
