c6d1988a8ca69df71faf83ce71ed73767c7d1199
Chris Hesterman <chrishesterman14@gmail.com>
2020-04-23 20:51:38 -0700

    Progress implementing get/post for users/messages

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,14 +2,42 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      models.messages
+        .get()
+        .then((data) => {
+          res.send(data);
+        })
+        .catch((err) => {
+          console.log('Error: ', err);
+          res.sendStatus(404);
+        });
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      models.messages
+        .post()
+        .then((data) => {
+          res.status(200).send(data);
+        })
+        .catch((err) => {
+          res.sendStatus(500);
+        });
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
+    get: function (req, res) {
+      models.users
+        .get()
+        .then((data) => {
+          res.send(data);
+        })
+        .catch((err) => {
+          console.log('Error: ', err);
+          res.sendStatus(404);
+        });
+    },
     post: function (req, res) {}
   }
 };
-
--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,74 @@
 var db = require('../db');
 
+var queryAllMessages = `SELECT messages.message, rooms.roomname, users.username
+  FROM messages, rooms, users
+  WHERE messages.rooms_id_room = rooms.id
+  AND messages.users_id_user = users.id`;
+
+var postMessage = `INSERT INTO messages (id, message, users_id_user, rooms_id_room, timestamp)
+  VALUES (null, 'Hi Hi Hi', 1, 1, NOW())`;
+
+var queryAllUsers = `SELECT users.username, rooms.roomname FROM users, rooms
+  WHERE users.rooms_id_currentRoom = rooms.id`;
+
+var checkUserExists = `CASE WHEN EXISTS (SELECT username FROM users WHERE username = 'Miss Piggy')
+  THEN 1
+  ELSE 0
+  END CASE`;
+
+var postUser = `INSERT INTO users (id, username, rooms_id_currentRoom) VALUES (null, )`;
+
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      return new Promise((resolve, reject) => {
+        db.query(queryAllMessages, (err, results) => {
+          if (err) {
+            return reject(err);
+          }
+
+          resolve(results);
+        });
+      });
+    }, // a function which produces all the messages
+    post: function () {
+      return new Promise((resolve, reject) => {
+        db.query(postMessage, (err, results) => {
+          if (err) {
+            return reject(err);
+          }
+
+          resolve('Message posted!');
+        });
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () {
+      return new Promise((resolve, reject) => {
+        db.query(queryAllUsers, (err, results) => {
+          if (err) {
+            return reject(err);
+          }
+
+          resolve(results);
+        });
+      });
+    },
+    post: function () {
+      return new Promise((resolve, reject) => {
+        db.query(checkUserExists, (err, results) => {
+          if (err) {
+            return reject(err);
+          } else {
+            if (results === 0) {
+              db.query();
+            }
+          }
+        });
+      });
+    }
   }
 };
-
